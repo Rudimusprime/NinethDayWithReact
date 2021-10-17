30d20c46ec53354f2a4c5ff9f3bb25d2bb93ec9a
DanielB <d.banasiewicz@pdo.com.pl>
2021-10-17 15:51:48 +0200

    Exchange counter done

---
+++ FirstExample/scripts/App.js
@@ -0,0 +1,46 @@
+const Dollars = (props) => (
+    <div>Wartość w dolarach: {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
+)
+
+const Euros = (props) => (
+    <div>Wartość w Euro: {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
+)
+
+class ExchangeCounter extends React.Component {
+
+    state = {
+        amount: "",
+        ratioDollar: 3.6,
+        ratioEuro: 4.6,
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            amount: e.target.value,
+        })
+    }
+
+    render() {
+
+        const {amount, ratioDollar, ratioEuro} = this.state;
+
+        return (
+            <div className="app">
+                <label>
+                    <input
+                        type="number"
+                        value={this.state.amount}
+                        onChange={this.handleChange}/>
+                </label>
+                <Dollars
+                    cash={amount}
+                    ratio={ratioDollar}/>
+                <Euros
+                    cash={amount}
+                    ratio={ratioEuro}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<ExchangeCounter/>, document.getElementById('root'));
